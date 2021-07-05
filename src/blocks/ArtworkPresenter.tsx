613b736b12b6bb3474a9c4a6e27a287d3d41a38b
zunaib <zunaib60@gmail.com>
2021-07-05 18:14:59 +0500

    Completed Artwork Presenter Structure

--- src/blocks/ArtworkPresenter.tsx
+++ src/blocks/ArtworkPresenter.tsx
@@ -5,15 +5,31 @@ const artworkPresenter = {
   category: 'Assessment',
   content: `
   <div class="art-board" >
-    <div class="bg-veil">
+  <div class="bg-veil">
+    <img data-gjs-type="image" src=""></img>
+  </div>
+  <div class="container">
+    <div class="left-container">
+      <img data-gjs-type="image" src=""></img>
+      <div class="img-txt">enter image text here!!</div>
     </div>
-    <div class="container">
-      <div class="left-container">
+    <div class="right-container">
+      <div class="custom-btn">
+        <div>X</div>
       </div>
-      <div class="right-container">
+      <div class="logo">
+        <img data-gjs-type="image" src=""></img>
+      </div>
+      <div class="artist-name">artist name</div>
+      <div class="art-name">art name</div>
+      <div class="art-date">art date</div>
+      <div class="art-desc">enter artwork description</div>
+      <div class="custom-btn enquire">
+        <div>Click Me</div>
       </div>
     </div>
   </div>
+</div>
   `+ ArtworkStyles,
   attributes: {
     class: "fa fa-square",
